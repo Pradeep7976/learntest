ce4321ba36ba5b5ff3b74a3ffec701871d181783
pradeep <phantompradeep200@gmail.com>
2022-06-01 09:55:26 +0530

    3nd one test

--- testii.js
+++ testii.js
@@ -1,8 +1,8 @@
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// const cors = require("cors");
-// const mongoose = require("mongoose");
-// const bcrypt = require("bcrypt");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const app = express();
 
@@ -69,6 +69,6 @@ app.post("/regesterr", async (req, res) => {
   }
 });
 
-// app.listen(7000, function () {
-//   console.log("Server started on port 7000");
-// });
+app.listen(7000, function () {
+  console.log("Server started on port 7000");
+});
